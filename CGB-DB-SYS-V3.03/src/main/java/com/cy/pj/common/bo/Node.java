cd5dbb3fce3bf1b131a56b7ea0c03ba5385eb2f7
wencaoskr <66624569+wencaoskr@users.noreply.github.com>
2020-06-16 21:52:27 +0800

    Update Node.java

--- CGB-DB-SYS-V3.03/src/main/java/com/cy/pj/common/bo/Node.java
+++ CGB-DB-SYS-V3.03/src/main/java/com/cy/pj/common/bo/Node.java
@@ -3,7 +3,7 @@ package com.cy.pj.common.bo;
 import java.io.Serializable;
 
 import lombok.Data;
-
+//POJO
 @Data
 public class Node implements Serializable{
 	private static final long serialVersionUID = 8556485044700393868L;
